ac17d7807009f3a5557f79a7ed5e756ac79dce8c
julia8873 <julia8873@correo.ugr.es>
2026-07-13 18:53:42 +0200

    refactor: reemplazar block_holamundo por block_gitmetrics

--- lang/en/block_holamundo.php
+++
@@ -1,6 +0,0 @@
-<?php
-$string['pluginname'] = 'Hello World';
-$string['nombreasaludar'] = 'Name to greet (per instance)';
-$string['nombredefecto'] = 'Default name to greet (global)';
-$string['holamundo:addinstance'] = 'Add a Hello World block';
-$string['contadortexto'] = 'Seen {$a} times';
--- lang/es/block_holamundo.php
+++
@@ -1,6 +0,0 @@
-<?php
-$string['pluginname'] = 'Hola Mundo';
-$string['nombreasaludar'] = 'Nombre a saludar (por instancia)';
-$string['nombredefecto'] = 'Nombre a saludar por defecto (global)';
-$string['holamundo:addinstance'] = 'Añadir un bloque Hola Mundo';
-$string['contadortexto'] = 'Visto {$a} veces';
